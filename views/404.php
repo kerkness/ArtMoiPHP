5753e590e2d909c55d8f64e70554dbd481d50a05
LJ Lee <lj@ninjaone.(none)>
2016-01-21 22:24:46 +0000

    fixed image url changed /portfolio/ArtMoiPHP/images/ or /images/ to /ArtMoiPHP/

--- views/404.php
+++ views/404.php
@@ -28,7 +28,7 @@
 <div class="text-center">
     <div class="errorMessage">
         <div class="col-md-offset-4 col-md-4 col-sm-3">
-            <img src="/images/whiteLogo.png" class="img-responsive">
+            <img src="/ArtMoiPHP/images/whiteLogo.png" class="img-responsive">
         </div>
         <div class="col-md-12 col-sm-12">
             <h1><?= $message ?></h1>
